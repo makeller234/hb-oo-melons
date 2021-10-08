da746934afa2b9f93fd3d931b5c77b0b390da8fd
Melissa Keller <melissa.keller23@gmail.com>
2021-10-08 11:16:16 -0500

    making absctract class and updating other classes

--- melons.py
+++ melons.py
@@ -1,7 +1,24 @@
 """Classes for melon orders."""
+class AbstractMelonOrder():
+        # def __init__(self):
+           
+            
+        def get_total(self):
+            """Calculate price, including tax."""
 
+            base_price = 5
+            if self.species == 'Christmas melons':
+                base_price = base_price * 1.5
+            total = (1 + self.tax) * self.qty * base_price
 
-class DomesticMelonOrder():
+            return total
+
+        def mark_shipped(self):
+            """Record the fact than an order has been shipped."""
+
+            self.shipped = True
+
+class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
 
     def __init__(self, species, qty):
@@ -13,21 +30,8 @@ class DomesticMelonOrder():
         self.order_type = "domestic"
         self.tax = 0.08
 
-    def get_total(self):
-        """Calculate price, including tax."""
 
-        base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
-
-        return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
-
-
-class InternationalMelonOrder():
+class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
 
     def __init__(self, species, qty, country_code):
@@ -44,14 +48,11 @@ class InternationalMelonOrder():
         """Calculate price, including tax."""
 
         base_price = 5
+        if self.qty < 10:
+            base_price = base_price + 3
         total = (1 + self.tax) * self.qty * base_price
 
         return total
-
-    def mark_shipped(self):
-        """Record the fact than an order has been shipped."""
-
-        self.shipped = True
 
     def get_country_code(self):
         """Return the country code."""
